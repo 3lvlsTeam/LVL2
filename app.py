431c223e5556819f9f741688c25bad7555dfe583
Victor <77194649+Zxeon13@users.noreply.github.com>
2021-05-01 02:12:45 +0200

    some updates

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-### import section ######################################################################################
+#-- import section ----------------------------------------------------------------------------------------------------------
 from functools import reduce
 import re , bcrypt,os
 from random import shuffle
@@ -7,25 +7,25 @@ from flask.globals import session
 from datetime import datetime, timedelta , date
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.utils import secure_filename
-from codes import pw_maker,how_strong,pinger
-###########################################################################################################
+from codes import functions
+#------------------------------------------------------------------------------------------------------------------------------
 
 
 
-## app configratins #######################################################################################
+#-- app configratins -----------------------------------------------------------------------------------------------------------
 app = Flask(__name__)
 app.secret_key="3Eg!hS_24vwvEWF34@!r"
 app.config['SQLALCHEMY_DATABASE_URI']= 'sqlite:///database/users.sqlite3'
 app.config['SQLALCHEMY_TRAC_MODIFICATIONS']=False
 app.permanent_session_lifetime = timedelta(days=1)
-###########################################################################################################
+
+#-------------------------------------------------------------------------------------------------------------------------------
 
 
 
 
-### db configratins ######################################################################################
+#-- db configratins ------------------------------------------------------------------------------------------------------------
 db = SQLAlchemy(app)
-
 class users(db.Model): 
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
@@ -37,6 +37,7 @@ class users(db.Model):
     user_password = db.Column(db.String(100),nullable=False)
     img_password = db.Column(db.String(100),nullable=True)
     signup_time = db.Column(db.DateTime)
+    tuple(db.UniqueConstraint('username', 'user_email'))
 
     def __init__(self,first_name,last_name,username,user_email,user_bday,user_password,signup_time,img_password):
         self.first_name=first_name
@@ -47,20 +48,40 @@ class users(db.Model):
         self.user_password=user_password
         self.signup_time=signup_time
         self.img_password=img_password
+    
+#-------------------------------------------------------------------------------------------------------------------------------
 
-###########################################################################################################
+
+#------  some functions  -------------------------------------------------------------------------------------------------------
+def make_tmp_usr():
+    try:
+        global usr
+        usr = users.query.filter_by(username=session["username"]).first()
+    except:pass
+def user_is_loged():
+    try:
+        usr = users.query.filter_by(username=session["username"]).first()
+        if session["username"]== usr.username :
+                if bcrypt.checkpw(session["password"],usr.user_password):
+                    if bcrypt.checkpw(session["img_password"],usr.img_password):
+                        return True
+    except:pass
+    return False
+#-------------------------------------------------------------------------------------------------------------------------------
+
+       
 
 
 
-### main function ########################################################################################
+#-- main function -------------------------------------------------------------------------------------------------------------
 @app.route("/")
 def main():
     return redirect(url_for("home"))
-###########################################################################################################
+#------------------------------------------------------------------------------------------------------------------------------
+    
 
 
-
-### signup function #######################################################################################
+#-- signup function -----------------------------------------------------------------------------------------------------------
 @app.route("/signup",methods=["POST","GET"])
 def signup():
     if request.method=="POST":
@@ -72,13 +93,16 @@ def signup():
         session["birthdate"]= datetime.strptime(request.form["input_birthdate"],'%Y-%m-%d')
         session["password"]= request.form["input_password1"]
         session["password2"]= request.form["input_password2"]
-        today=to_integer(date.today())
-        birthdate=to_integer(session["birthdate"])
+        today=functions.age.to_integer(date.today())
+        birthdate=functions.age.to_integer(session["birthdate"])
         age=today-birthdate
         tempuser= users.query.filter_by(username=session["username"]).first()
-        
         if tempuser:
             flash("username alrady existe, try another one.")
+            allgood= False
+        email= users.query.filter_by(user_email=session["useremail"]).first()
+        if email:
+            flash("This Email is alrady used, try another one.")
             allgood= False
         if re.search(r'\d', session["firstname"]):
             flash(" fits name must be letters only")
@@ -89,7 +113,7 @@ def signup():
         if re.search(r'[^a-zA-Z0-9]', session["username"]):
             flash("username  must be letters and numbers only")
             allgood=False
-        if  pinger.test_if_real(session["useremail"]):
+        if  functions.pinger.test_if_real(session["useremail"]):
             flash("email is not valid")
             allgood=False
         if age < 180000:
@@ -98,7 +122,7 @@ def signup():
         if session["password"] != session["password2"]:
             flash("passowrs dont match")
             allgood=False
-        if how_strong.how_strong(session["password"]) < 500:
+        if functions.how_strong.how_strong(session["password"]) < 500:
             flash("passowr are too weak ")
             allgood=False
         if allgood:
@@ -107,83 +131,73 @@ def signup():
             newuser=users(session["firstname"],session["lastname"],session["username"],session["useremail"],session["birthdate"],session["password_crybted"],datetime.now(),None)
             db.session.add(newuser)
             db.session.commit()
+            make_tmp_usr()
             return redirect(url_for("signupF2"))
 
     return render_template("signup.html")
 
-def to_integer(dt_time):
-    return 10000*dt_time.year + 100*dt_time.month + dt_time.day
-#############################################################################################################
+
+#--------------------------------------------------------------------------------------------------------------------------------
 
 
-##### signup function 2#######################################################################################
+#---- signup 2 function ----------------------------------------------------------------------------------------------------------
 @app.route("/signupF2",methods=["POST","GET"])
 def signupF2():   
-    usr = users.query.filter_by(username=session["username"]).first()
     if request.method == "POST":
         session["img_password"]=request.form["img_password"]
         if session['img_password'].count(".")>4:
             session["img_password"]=session["img_password"].encode("utf-8")
-            tmp_img_password=bcrypt.hashpw(session["img_password"],bcrypt.gensalt())
-            usr.img_password = tmp_img_password
-            tmp_img_password=""
+            usr.img_password=bcrypt.hashpw(session["img_password"],bcrypt.gensalt())
             db.session.commit()
             flash('Done!')
             return redirect(url_for("home"))
-        else:flash("click 5 at lest!!")
-    directory="static/images/"+str(usr.id)
-    if not os.path.exists(directory):
-        os.makedirs(directory)
-    
-    img_list =  os.listdir(directory)
-    if len(img_list) <5:
-        flash("you need 2 have at lest 5 photos for the key, will use defalt photos 4 now.")
-        directory="static/images/defalt"
-    img_list =  os.listdir(directory)
-    shuffle(img_list)
+        else:flash("Choose 5 images at lest!!")
+    directory=functions.directory.directory_maker(usr.id)
+    img_list= functions.directory.directory_scaner(directory)
     return render_template("signupF2.html", imgs_list =img_list,directory=directory , username=usr.username)
-###########################################################################################################
+#------------------------------------------------------------------------------------------------------------------------------
 
 
-### pwget function ########################################################################################
+#---- uploader function --------------------------------------------------------------------------------------------------------
+
+#-------------------------------------------------------------------------------------------------------------------------------
+
+
+
+#--- pwget function ------------------------------------------------------------------------------------------------------------
 @app.route("/pwgen",methods=["POST","GET"] )
 def pwgen():
     
     if request.method == "POST":
-        
         input_gen_numbers = request.form["input_gen_numbers"]
         input_gen_text = request.form["input_gen_text"]
-        if pw_maker.passwordgenerator.conventer_to_list(input_gen_text) < 8:
+        if functions.pw_maker.conventer_to_list(input_gen_text) < 8:
             flash("enter al lest 8 words")
             return redirect(url_for('pwgen'))
-        elif pw_maker.passwordgenerator.conventer_to_list(input_gen_text) > 7:
-            gen_password = pw_maker.passwordgenerator.password_maker(input_gen_numbers,input_gen_text)
+        elif functions.pw_maker.conventer_to_list(input_gen_text) > 7:
+            gen_password = functions.pw_maker.password_maker(input_gen_numbers,input_gen_text)
             return render_template("pwgen.html",gen_password=gen_password) 
-            
 
     return render_template("pwgen.html")
-###########################################################################################################
+#-------------------------------------------------------------------------------------------------------------------------------
 
 
 
 
 
-### login function #######################################################################################
+#--- login function -----------------------------------------------------------------------------------------------------------
 @app.route("/login",methods=["POST","GET"])
 def login():
     try:
-        usr = users.query.filter_by(username=session["username"]).first()
         if bcrypt.checkpw(session["password"],usr.user_password):
-            flash("You loged in alrady!")
             return redirect(url_for("loginF2") )
     except:
         pass
-
     if request.method=="POST":
         session.clear()
         session["username"]=request.form["username"]
         session["password"]=request.form["password"].encode("utf-8")
-        usr = users.query.filter_by(username=session["username"]).first()
+        make_tmp_usr()
         if usr:
             if bcrypt.checkpw(session["password"],usr.user_password):
                 return redirect(url_for("loginF2") )
@@ -195,13 +209,12 @@ def login():
         flash("wrong username or password.")
     return render_template("login.html")
     
-##########################################################################################################
+#------------------------------------------------------------------------------------------------------------------------------
 
 
-### loginF2 function ######################################################################################
+#--- loginF2 function ----------------------------------------------------------------------------------------------------------
 @app.route('/loginF2', methods=['GET', 'POST'])
 def loginF2():
-    usr = users.query.filter_by(username=session["username"]).first()
     try:
         if session["img_password"]== usr.img_password:
             return redirect(url_for("home") )
@@ -216,60 +229,49 @@ def loginF2():
         if bcrypt.checkpw(session["img_password"],usr.img_password):
             return redirect(url_for("home") )
         else:
-            flash("wrong order!!")
-
-
-    directory="static/images/"+str(usr.id)
-    img_list =  os.listdir(directory)
-    if len(img_list) <5:
-            flash("you need 2 have at lest 5 photos for the key, will use defalt photos 4 now.")
-            directory="static/images/defalt"
-            img_list =  os.listdir(directory)
-    shuffle(img_list)
+            flash("wrong order,pleas try again!")
+    
+    directory=functions.directory.directory_maker(usr.id)
+    img_list= functions.directory.directory_scaner(directory)
     return render_template("loginF2.html", imgs_list =img_list, directory=directory ,username=usr.username )
-###########################################################################################################
+#-------------------------------------------------------------------------------------------------------------------------------
   
 
 
 
-### home function ########################################################################################
+#-- home function ------------------------------------------------------------------------------------------------------------
 @app.route("/home")
 def home():
-    try:
-        usr = users.query.filter_by(username=session["username"]).first()
-        if session["username"]== usr.username :
-            if bcrypt.checkpw(session["password"],usr.user_password):
-                if bcrypt.checkpw(session["img_password"],usr.img_password):
-                    return render_template("home.html", username=usr.username)
-    except:pass
-    flash("login first.")
-    return redirect(url_for("login"))
-###########################################################################################################
+    if user_is_loged():
+        return render_template("home.html", username=usr.username)
+    else:
+        flash("You r not loged in!")
+        return redirect(url_for("login"))
+#------------------------------------------------------------------------------------------------------------------------------
 
 
-#### profile function #####################################################################################
+#--- profile function ---------------------------------------------------------------------------------------------------------
 @app.route("/profile")
 def profile():
-    usr = users.query.filter_by(username=session["username"]).first()
-    if session["username"]== usr.username :
-        if bcrypt.checkpw(session["password"],usr.user_password):
-            if bcrypt.checkpw(session["img_password"],usr.img_password):
-                return render_template("profile.html",usr=usr,username=usr.username)
-    else: return redirect(url_for("home"))
+    if user_is_loged():
+        return render_template("profile.html",usr=usr)
+    else:
+        flash("You r not loged in!")
+        return redirect(url_for("login"))
     
-###########################################################################################################
+#------------------------------------------------------------------------------------------------------------------------------
 
 
-### logout function #######################################################################################
+#--- logout function -----------------------------------------------------------------------------------------------------------
 @app.route("/logout")
 def logout():
     session.clear()
     flash("you just loged out")
     return redirect(url_for("main"))
-###########################################################################################################
+#------------------------------------------------------------------------------------------------------------------------------
 
 
-### code runner function ###################################################################################
+#--- code runner function -------------------------------------------------------------------------------------------------------
 if __name__ == "__main__":
     db.create_all()
     app.run(debug=True)
--- codes/pinger.py
+++
@@ -1,9 +0,0 @@
-from ping3  import ping
-
-def test_if_real(email):
-    temp=str(email).split("@")
-    link=str(temp[1])
-    if ping(link):
-        return False
-    else:
-        return True
